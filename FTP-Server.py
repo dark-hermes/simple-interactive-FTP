e08d2129d958c96fb8b82721aa76d87bd977a7f3
Hermawan Sentyaki <52445704+hermawans@users.noreply.github.com>
2020-01-01 12:19:35 +0700

    Create FTP-Server.py

---
+++ FTP-Server.py
@@ -0,0 +1,18 @@
+#install pyftpdlib library module before start
+#pip install ftpdlib
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("mawan", "nevtik", "/home/username", perm="elradfmw") #You can use permission as you wish
+authorizer.add_user("github", "12345", "/home/username", perm="elradfmw")
+authorizer.add_anonymous("/home/username", perm="elradfmw") #For anonymous (Guest) user
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+host, port = input("Input Host : "),int(input("Input Port[21] :")
+server = FTPServer(('host','port'), handler) 
+server.serve_forever()
