299d45c9f8be38fb3d1949e0fd417c283dbd5f7d
Hermawan Sentyaki <52445704+hermawans@users.noreply.github.com>
2020-01-01 18:52:41 +0700

    Update FTP-Server.py

--- FTP-Server.py
+++ FTP-Server.py
@@ -1,19 +1,18 @@
-#install pyftpdlib library module and pip before start\
-#apt install python-pip or apt install python3-pip
-#pip install ftpdlib
+#install ftpflib library and pip before start 
+#apt install python3-pip && pip install ftpdlib
 
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
-authorizer.add_user("mawan", "nevtik", "/home/username", perm="elradfmw") #change permission and directory as you wish
-authorizer.add_user("github", "12345", "/home/username", perm="elradfmw")
+authorizer.add_user("mawan13", "130504", "Server", perm="elradfmw") #Change "Server" with your directory
+authorizer.add_user("github","12345", "Server", perm="elradfmw") #You can change user permission as you wish
 authorizer.add_anonymous("/home/username", perm="elradfmw") #For anonymous (Guest) user
 
 handler = FTPHandler
 handler.authorizer = authorizer
 
-host, port = input("Input Host : "),int(input("Input Port[21] :"))
-server = FTPServer(('host','port'), handler) 
+host, port = input("Input Host : "), int(input("Port : "))
+server = FTPServer((host,port), handler)
 server.serve_forever()
