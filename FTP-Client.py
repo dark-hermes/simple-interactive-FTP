788de3358485181be7de8497a6337058edf1ab3a
Hermawan Sentyaki <52445704+hermawans@users.noreply.github.com>
2020-01-01 18:46:53 +0700

    Update FTP-Client.py

--- FTP-Client.py
+++ FTP-Client.py
@@ -1,33 +1,42 @@
-from ftplib import FTP
+import ftplib #import ftplib module
 
-ftp = FTP('')
-host, port = input("Input Server Host : "), int(input("Input Server Port[21] : "))
-ftp.connect('host','port')
-ftp.login()
+ftp = ftplib.FTP('') #save FTP func as ftp var
+host = input    ("Input Host Server    : ") 
+port = int(input("Input Port Server    : "))
 
-def upload():
- ftp.retrlines('LIST')
- filename = input("\nWhich file you like to upload : ") #Input a file name with it's extension in the same directory with this file or you can use relative URL
- ftp.storbinary('STOR '+filename, open(filename, 'rb'))
- ftp.quit()
-
-def download():
- ftp.retrlines('LIST')
- filename = input("\nWhich file you like to download : ") #Input a file name with it's extension in server directory
- localfile = open(filename, 'wb')
- ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
- ftp.quit()
- localfile.close()
-
-choice = input("What do you want :\n1. Upload File\n2. Download File\n3. Quit\nYour Choice : ")
-if((choice == 1)or(choice=="Upload")or(choice=="upload")or(choice=="UPLOAD")):
- upload()
-
-elif((choice==2)or(choice=="download")or(choice=="DOWNLOAD")or(choice=="Download")):
- download()
- 
-elif(choice==3)or(choice=="Quit")or(choice=="quit")or(choice=="QUIT"):
- ftp.quit()
- 
+ftp.connect(host,port)
+username, passwords = input("\nUsername             : "),input("Password             : ")
+ftp.login(user = username,passwd= passwords)
 
 
+
+choice = input("What do you want :\n1. Upload\n2. Download\n3. Quit\nInput : ")
+if choice=="Upload" or choice=="UPLOAD" or choice == "upload" or choice==1:
+    def upload():
+        ftp.retrlines('LIST')
+        filename = input("Pick a file : ")
+        ftp.storbinary('STOR '+filename, open(filename, 'rb'));
+        print("\n"+filename,"uploaded successfully");
+        ftp.quit()
+    try:
+        upload()
+    except FileNotFoundError:
+        print("\nUpload Failed")
+        ftp.quit()
+
+elif choice=="Download" or choice == "download" or choice == "DOWNLOAD" or choice == 2:
+    def download():
+        ftp.retrlines('LIST')
+        filename = input("Pick a file from server : ")
+        localfile = open(filename, 'wb')
+        ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+        print("\n"+filename,"downloaded successfully")
+        ftp.quit()
+        localfile.close()
+    try:
+        download()
+    except ftplib.error_perm:
+        print("\nDownload Failed")
+
+elif choice == "Quit" or choice == "quit" or choice == "QUIT" or choice == 3:
+    print("\nProgram Finished");ftp.quit();
